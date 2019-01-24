ab28476527bf27a622bc578f2bc5781fae0a3300
RaissonCarvalho <raissoncarvalho94@gmail.com>
2019-01-24 14:45:20 -0300

    Modelando classe Disciplina e começando configuração objectbox

--- app/src/main/java/com/acompanhamentoescolar/activity/MainActivity.java
+++ app/src/main/java/com/acompanhamentoescolar/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.acompanhamentoescolar;
+package com.acompanhamentoescolar.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.acompanhamentoescolar.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/acompanhamentoescolar/model/Disciplina.java
@@ -0,0 +1,56 @@
+package com.acompanhamentoescolar.model;
+
+import io.objectbox.annotation.Entity;
+import io.objectbox.annotation.Id;
+
+@Entity
+public class Disciplina {
+
+    @Id
+    private long id;
+    private String nome;
+    private double media;
+    private String situacao;
+
+    public Disciplina(){
+
+    }
+
+    public Disciplina(String nome, double media, String situacao) {
+        this.nome = nome;
+        this.media = media;
+        this.situacao = situacao;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+}
